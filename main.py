5226459795425cf1ad70771a4e0f4f6420d91a01
Chris-W-B <survivios198@gmail.com>
2022-12-18 18:58:08 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import sqlite3
 import tkinter
 import tkinter.ttk
+import tkinter as tk
+import tkinter.messagebox
+
 # By Team Blue
 
 mainwindow = tkinter.Tk()
@@ -13,42 +16,28 @@ info = tkinter.StringVar()
 InsertClicked = False
 UpdateStudentClicked = False
 OutLabel = tkinter.Label(BottomFrame, textvariable=info)
-LeftOverButtons = False
 
 
-
-def main(): #Creates database and containes first steps of the GUI interface
+def main():  # Creates database and containes first steps of the GUI interface
     curs.execute('''CREATE TABLE IF NOT EXISTS StudentDatabase(student_id INTEGER PRIMARY KEY NOT NULL, 
-                first_name TEXT NOT NULL,last_name TEXT NOT NULL, course_one TEXT NOT NULL, 
-                course_two TEXT NOT NULL, hobby_one TEXT NOT NULL, hobby_two TEXT NOT NULL)''')
+                first_name TEXT NOT NULL,last_name TEXT NOT NULL, age INTEGER NOT NULL, 
+                state TEXT NOT NULL, hobby_one TEXT NOT NULL, hobby_two TEXT NOT NULL)''')
 
-def Database(): #Creates database and containes first steps of the GUI interface
-    curs.execute('''CREATE TABLE IF NOT EXISTS StudentDatabase(student_id INTEGER PRIMARY KEY NOT NULL,first_name TEXT NOT NULL,
-                last_name TEXT NOT NULL, course_one TEXT NOT NULL, course_two TEXT NOT NULL, hobby_one TEXT NOT NULL,
-                hobby_two TEXT NOT NULL)''')
-        #This is the actual database, you guys can just change the ###### to whatever you want
-    curs.execute('''INSERT INTO TABLE StudentDatabase("00000001", "William", "Brando", "16", 
-        "MN", "Gaming", "Building Models")''')
-    curs.execute('''INSERT INTO TABLE StudentDatabase("00000002", "Christian", "Berry", "########", 
-        "########", "########", "#######")''')
-    curs.execute('''INSERT INTO TABLE StudentDatabase("00000003", "Mason", "Baloun", "########", 
-        "#########", "########", "##########")''')
-    curs.execute('''INSERT INTO TABLE StudentDatabase("00000004", "David", "Conway", "17", 
-        "MN", "Art", "Creating Graphic Novels")''')
-    curs.execute('''INSERT INTO TABLE StudentDatabase("00000005", "Jacob", "Howler", "16", 
-        "AZ", "Books", "Making Things")''')
-    curs.execute('''INSERT INTO TABLE StudentDatabase("00000006", "Yuuji", "Itadori", "16", 
-        "FL", "Watching Anime", "Hanging out with Friends")''')
-    curs.execute('''INSERT INTO TABLE StudentDatabase("00000007", "Jay", "Dipersia", "21", 
-        "IL", "Playing Murder Mystery", "Art")''')
-    curs.execute('''INSERT INTO TABLE StudentDatabase("00000008", "Ikora", "Rey", "19", 
-        "MD", "Books", "Studying the Stars")''')
-    curs.execute('''INSERT INTO TABLE StudentDatabase("00000009", "Isaac", "Nelson", "16", 
-        "CA", "Gaming", "Singing")''')
-    curs.execute('''INSERT INTO TABLE StudentDatabase("00000010", "Alex", "Balken", "22", 
-        "WY", "Welding", "Wood Carving")''')
-    curs.execute('''SELECT * From StudentDatabase''')
-    Info = tkinter.Label(TopFrame, text="Student Database. Please select Input to input more data, \n Read to read existing data, or Close to save and exit the program.")
+    # This is the actual database, you guys can just change the ###### to whatever you want
+    people_info = [(1, 'Mason', 'Baloun', 18, 'MN', 'Art', 'Running'),
+                   (2, 'William', 'Brando', 16, 'MN', 'Gaming', 'Building Models'),
+                   (3, 'Christian', 'Berry', 18, 'MN', 'Art', 'Running'),
+                   (4, 'Joe', 'Mama', 18, 'MN', 'Art', 'Running'),
+                   (5, 'Yuuji', 'Itadori', 19, 'NY', 'Watching Anime', 'Hanging Out With Friends'),
+                   (6, 'Jay', 'Dipersia', 21, 'IL', 'Playing Murder Mystery', 'Art'),
+                   (7, 'Ikora', 'Rey', 20, 'PA', 'Reading', 'Stargazing'),
+                   (8, 'Isaac', 'Nelson', 16, 'MD', 'Music', 'Exercise'),
+                   (9, 'Alex', 'Balken', 18, 'WY', 'Welding', 'Wood Carving'),
+                   (10, 'David', 'Conway', 22, 'AZ', 'Reading', 'Gaming'), ]
+    curs.executemany('INSERT OR REPLACE INTO StudentDatabase VALUES (?, ?, ?, ?, ?, ?, ?)', people_info)
+
+    Info = tkinter.Label(TopFrame,
+                         text="Student Database. Please select Input to input more data, \n Read to read existing data, or Close to save and exit the program.")
     InputButton = tkinter.Button(TopFrame, text="Input", command=Insert)
     ReadButton = tkinter.Button(TopFrame, text="Read", command=Request)
 
@@ -69,20 +58,33 @@ def Database(): #Creates database and containes first steps of the GUI interface
     conn.close()
 
 
-def Request(): #Gives all the data in the database
-    global LeftOverButtons
-    if LeftOverButtons == True:
-        List = str(curs.fetchone())
-    while List != "None":
-        List += "\n"
-        List += str(curs.fetchone())
-    OutLabel.pack()
-    info.set(List)
+def Request():  # Show all data inside SQL table
+    tkinter = tk.Tk()
+    tkinter.geometry("660x250")
+    r_set = conn.execute('''SELECT * from StudentDatabase LIMIT 0,10''');
+    e = tk.Entry(tkinter, width=15, fg='black')
+
+    student_fields = ('Student ID', "First Name", "Last Name", 'Age', "State", "Hobby One", "Hobby Two")
+    s = 0
+    for i in range(7):
+        e = tk.Entry(tkinter, width=15, fg='black')
+        e.grid(row=0, column=i)
+        e.insert(tk.END, student_fields[s])
+
+        s += 1
+
+    r = 1
+    for student in r_set:
+        for c in range(len(student)):
+            e = tk.Entry(tkinter, width=15, fg='black')
+            e.grid(row=r + 1, column=c)
+            e.insert(tk.END, student[c])
+
+        r = r + 1
 
 
-def Insert(): #Hub for changing student information
-    global InsertClicked, LeftOverButtons
-    LeftOverButtons = True
+def Insert():  # Hub for changing student information
+    global InsertClicked
     info.set("Would you like to update existing data, delete an existing student, or create a new student?")
     OutLabel.pack()
     if InsertClicked == False:
@@ -93,13 +95,15 @@ def Insert(): #Hub for changing student information
             UpdateData()
 
         UpdateDataButton = tkinter.Button(BottomFrame, text="Update Data", command=UpdateSuicide)
+
         def NewStudentSuicide():
             DeleteStudentButton.destroy()
             UpdateDataButton.destroy()
             NewStudentButton.destroy()
             NewStudent()
 
-        NewStudentButton = tkinter.Button(BottomFrame, text="Create New Student", command=NewStudentSuicide)
+        NewStudentButton = tkinter.Button(BottomFrame, text="Create New Student", command=NewStudent)
+
         def DeleteSuicide():
             DeleteStudentButton.destroy()
             UpdateDataButton.destroy()
@@ -113,8 +117,105 @@ def Insert(): #Hub for changing student information
         InsertClicked = True
 
 
-
 def UpdateData():
+    global UpdateStudentClicked
+    if UpdateStudentClicked == False:
+        window = tkinter.Tk()
+        window.title("Data Entry Form")
+        curs.execute('''Select * FROM StudentDatabase''')
+
+        IDText = tkinter.Label(BottomFrame, text="Please enter the appropriate Student ID")
+        StudentInput = tkinter.Entry(BottomFrame)
+
+        def Give():
+            InfoChange()
+
+        EnterButton = tkinter.Button(BottomFrame, text="Enter", command=Give)
+        OutLabel.pack()
+        IDText.pack()
+        StudentInput.pack()
+        EnterButton.pack()
+
+        def InfoChange():
+            StudentID = StudentInput.get()
+            frame = tkinter.Frame(window)
+            frame.pack()
+
+            # Saving User Info
+            info_frame = tkinter.LabelFrame(frame, text="Add Student Information")
+            info_frame.grid(row=0, column=0, padx=15, pady=20)
+
+            first_name_label = tkinter.Label(info_frame, text="First Name")
+            first_name_label.grid(row=0, column=0)
+
+            last_name_label = tkinter.Label(info_frame, text="Last Name")
+            last_name_label.grid(row=0, column=1)
+
+            hobby_one_label = tkinter.Label(info_frame, text="Hobby One")
+            hobby_one_label.grid(row=0, column=1)
+
+            # Entry's
+            first_name_entry = tkinter.Entry(info_frame)
+            first_name_entry.grid(row=1, column=0)
+
+            last_name_entry = tkinter.Entry(info_frame)
+            last_name_entry.grid(row=1, column=1)
+
+            hobby_one_entry = tkinter.Entry(info_frame)
+            hobby_one_entry.grid(row=1, column=0)
+
+            # Age Number Box
+            age_label = tkinter.Label(info_frame, text="Age")
+            age_spinbox = tkinter.Spinbox(info_frame, from_=18, to=110)
+            age_label.grid(row=2, column=0)
+            age_spinbox.grid(row=3, column=0)
+
+            # State Information labels
+            state_label = tkinter.Label(info_frame, text="State")
+            state_combobox = tkinter.ttk.Combobox(info_frame,
+                                                  values=['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL',
+                                                          'GA',
+                                                          'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD',
+                                                          'ME',
+                                                          'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ',
+                                                          'NM',
+                                                          'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN',
+                                                          'TX',
+                                                          'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY'])
+
+            state_label.grid(row=2, column=1)
+            state_combobox.grid(row=3, column=1)
+
+            def enter_updated_info():
+                firstname = first_name_entry.get()
+                lastname = last_name_entry.get()
+                state = state_combobox.get()
+                age = age_spinbox.get()
+                hobbyOne = hobby_one_entry.get()
+
+                conn = sqlite3.connect('StudentDatabase.db')
+
+                curs.execute('UPDATE StudentDatabase SET first_name = ?, last_name=?, age=?, state=? '
+                             'WHERE student_id=?',
+                             (firstname, lastname, state, age, StudentID))
+
+                conn.commit()
+
+            # Buttons
+            UpdateStudentClicked = True
+            button = tkinter.Button(frame, text="Enter data", command=enter_updated_info)
+            button.grid(row=3, column=0, sticky="news", padx=20, pady=10)
+            window.mainloop()
+
+        Change()
+
+
+def Change():
+    global UpdateStudentClicked
+    UpdateStudentClicked = False
+
+
+def NewStudent():
     global UpdateStudentClicked
     if UpdateStudentClicked == False:
         window = tkinter.Tk()
@@ -166,14 +267,10 @@ def UpdateData():
 
             conn = sqlite3.connect('StudentDatabase.db')
 
-            student_info = (1, firstname, lastname)
-
-
-            curs.execute('INSERT OR REPLACE INTO Something VALUES (?, ?, ?)', student_info)
+            student_info = (11, firstname, 'Baloun', 18, 'MN', 'Art', 'Running')
+            curs.execute('INSERT INTO StudentDatabase VALUES (?, ?, ?, ?, ?, ?, ?)', student_info)
 
             conn.commit()
-
-            print(1, firstname, lastname)
 
         # Buttons
         UpdateStudentClicked = True
@@ -184,49 +281,40 @@ def UpdateData():
         Change()
 
 
-
-
-def Change():
-    global UpdateStudentClicked
-    UpdateStudentClicked = False
-
-def NewStudent():
-    pass
-
-
 def DeleteStudent():
-    curs.execute('''SELECT student_id, first_name, last_name FROM StudentDatabase''')
-    text = str(curs.fetchone())
-    info.set(text)
-    while text != "None":
-        text += "\n"
-        text += str(curs.fetchone())
+    curs.execute('''Select * FROM StudentDatabase''')
+    OutLabel.pack()
 
-    text += "\nPlease enter the student id of the student you would like to remove from the database"
     def Grab():
-        StudentId=StudentIdEnter.get()
+        StudentId = StudentIdEnter.get()
         EnterButton.destroy()
         StudentIdText.destroy()
 
         StudentIdEnter.destroy()
         info.set(f"Are you sure you would like to permenantly delete the information about {StudentId}?")
         OutLabel.pack()
+
         def FinalDeleteStudent():
             curs.execute('''DELETE FROM StudentDatabase WHERE student_id == ?''', StudentId)
+            tkinter.messagebox.showinfo('Response', f'Deleted StudentID:{StudentId} ')
 
         YesButton = tkinter.Button(BottomFrame, text="Yes", command=FinalDeleteStudent)
 
-        NoButton = tkinter.Button(BottomFrame, text="No")
+        NoButton = tkinter.Button(BottomFrame, text="No", command=Reset)
         YesButton.pack(side="left")
         NoButton.pack(side="right")
 
-    info.set(text)
     StudentIdText = tkinter.Label(BottomFrame, text="Student ID:")
     StudentIdEnter = tkinter.Entry(BottomFrame)
     EnterButton = tkinter.Button(BottomFrame, text="Enter", command=Grab)
     StudentIdText.pack(side="left")
     StudentIdEnter.pack(side="left")
     EnterButton.pack()
+
+
+def Reset():
+    mainwindow.destroy()
+    main()
 
 
 if __name__ == '__main__':
